f78cf7e4081410ba0e041f70cb63cee3294bae06
Tomasz <Tomasz.Szydlowski@solvti.pl>
2021-07-24 19:30:24 +0200

    06-solution-all-test-passed

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,15 +1,27 @@
 import {FC, useState} from "react";
+import "./Counter.css"
 
 export const Counter: FC = () => {
     const [counterValue, setCounterValue] = useState(0);
     const [inputValue, setInputValue] = useState(1);
+
+    const subtractFromCounter = () => {
+        setCounterValue(counterValue - inputValue)
+    };
+
+    const addFromCounter = () => {
+        setCounterValue(counterValue + inputValue)
+    }
+
     return (
         <div>
             <h3 data-testid={"header"}>My counter</h3>
-            <h2 data-testid={"counter"}>{counterValue}</h2>
-            <button data-testid={"minus-btn"}>-</button>
-            <input data-testid={"input"} type={"number"} value={inputValue}/>
-            <button data-testid={"add-btn"}>+</button>
+            <h2 data-testid={"counter"}
+                className={`${counterValue >= 100 ? "green" : ""}${counterValue <= -100 ? "red" : ""}`}>{counterValue}</h2>
+            <button data-testid={"minus-btn"} onClick={subtractFromCounter}>-</button>
+            <input data-testid={"input"} type={"number"} value={inputValue} className={"text-center"}
+                   onChange={(e) => setInputValue(parseInt(e.target.value))}/>
+            <button data-testid={"add-btn"} onClick={addFromCounter}>+</button>
         </div>
     )
-}
+} 
