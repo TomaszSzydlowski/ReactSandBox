cc4c2de9b634e4648a0ae90410063dcc5bd0f920
Tomasz <Tomasz.Szydlowski@solvti.pl>
2021-07-24 18:06:55 +0200

    04-test-passing

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,9 +1,15 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 
 export const Counter: FC = () => {
+    const [counterValue, setCounterValue] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
     return (
         <div>
-            <h1 data-testid="header">My counter</h1>
+            <h3 data-testid={"header"}>My counter</h3>
+            <h2 data-testid={"counter"}>{counterValue}</h2>
+            <button data-testid={"minus-btn"}>-</button>
+            <input data-testid={"input"} type={"number"} value={inputValue}/>
+            <button data-testid={"add-btn"}>+</button>
         </div>
     )
 }
